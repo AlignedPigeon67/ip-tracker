71f063bc0227534cc8c2314b268af791672fe512
Tom Hine <tomhine792@gmail.com>
2021-06-28 12:57:06 +0100

    add red background to input box on fetch error

--- main.js
+++ main.js
@@ -1,31 +1,31 @@
-const ipInputForm = document.querySelector('[data-ip-submit-form]');
-const ipInputElement = document.querySelector('[data-ip-submit-input');
+const ipInputForm = document.querySelector("[data-ip-submit-form]");
+const ipInputElement = document.querySelector("[data-ip-submit-input");
 
 let defaultPos = [51.505, -0.09];
 
 const api = {
-  base: 'https://geo.ipify.org/api/v1',
-  key: 'at_MNhCpJqYqt5IvXtASlsh2COfqyrBp',
+  base: "https://geo.ipify.org/api/v1",
+  key: "at_MNhCpJqYqt5IvXtASlsh2COfqyrBp",
 };
 
-let mymap = L.map('mapid', { zoomControl: false }).setView(defaultPos, 13);
+let mymap = L.map("mapid", { zoomControl: false }).setView(defaultPos, 13);
 
 L.tileLayer(
-  'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}',
+  "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
   {
     attribution:
       'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
-    id: 'mapbox/streets-v11',
+    id: "mapbox/streets-v11",
     tileSize: 512,
     zoomOffset: -1,
     accessToken:
-      'pk.eyJ1IjoiYWxpZ25lZHBpZ2VvbjY3IiwiYSI6ImNrcWMwdmthZDAwNGsydnBhMndueWV5ZGcifQ.sa-3aL9uYsqMmMfvekr9Lw',
+      "pk.eyJ1IjoiYWxpZ25lZHBpZ2VvbjY3IiwiYSI6ImNrcWMwdmthZDAwNGsydnBhMndueWV5ZGcifQ.sa-3aL9uYsqMmMfvekr9Lw",
   }
 ).addTo(mymap);
 
 let icon = L.icon({
-  iconUrl: './images/icon-location.svg',
+  iconUrl: "./images/icon-location.svg",
   iconSize: [46, 56],
   iconAnchor: [23, 55],
 });
@@ -38,11 +38,11 @@ const relocate = (pos, zoom = 13) => {
   marker.setLatLng(pos);
 };
 
-const populateInfo = ipData => {
-  const infoIp = document.querySelector('[data-info-ip]');
-  const infoLocation = document.querySelector('[data-info-location]');
-  const infoTimezone = document.querySelector('[data-info-timezone]');
-  const infoIsp = document.querySelector('[data-info-isp]');
+const populateInfo = (ipData) => {
+  const infoIp = document.querySelector("[data-info-ip]");
+  const infoLocation = document.querySelector("[data-info-location]");
+  const infoTimezone = document.querySelector("[data-info-timezone]");
+  const infoIsp = document.querySelector("[data-info-isp]");
 
   const loc = ipData.location;
 
@@ -52,14 +52,28 @@ const populateInfo = ipData => {
   infoIsp.innerText = ipData.isp;
 };
 
-const fetchIpLocation = async (query = '') => {
+const handleFetchLocationError = (err) => {
+  // const
+  // alert("Invalid IP address or domain input");
+  ipInputElement.classList.add("error");
+  setTimeout(() => {
+    ipInputElement.classList.remove("error");
+  }, 3000);
+  console.table(err);
+};
+
+const fetchIpLocation = async (query = "") => {
+  let isIpFail = false;
+  let currentErr;
+
   try {
     const res = await fetch(`${api.base}?apiKey=${api.key}&ipAddress=${query}`);
     const data = await res.json();
     relocate([data.location.lat, data.location.lng]);
     populateInfo(data);
   } catch (err) {
-    console.error(err);
+    isIpFail = true;
+    currentErr = err;
   }
 
   try {
@@ -68,14 +82,14 @@ const fetchIpLocation = async (query = '') => {
     relocate([data.location.lat, data.location.lng]);
     populateInfo(data);
   } catch (err) {
-    console.error(err);
+    if (isIpFail) handleFetchLocationError([err, currentErr]);
   }
 };
 
-ipInputForm.addEventListener('submit', e => {
+ipInputForm.addEventListener("submit", (e) => {
   e.preventDefault();
   fetchIpLocation(ipInputElement.value);
-  ipInputElement.value = '';
+  ipInputElement.value = "";
 });
 
 fetchIpLocation();
